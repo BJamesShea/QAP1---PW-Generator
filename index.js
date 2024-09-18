c489b57b151b9aef7531a63bc6ac246f838a708b
Brandon Shea <brandon.shea1994@icloud.com>
2024-09-18 20:43:02 -0230

    remove customization for pull request

--- index.js
+++ index.js
@@ -48,22 +48,6 @@ if (
     console.log("Invalid length value. Using default length of 8.");
     process.exit(1);
   }
-
-  // extra features for brownie points
-
-  if (arguments.includes("--numbers") || arguments.includes("-n")) {
-    lowerCase += numbers;
-  }
-
-  if (arguments.includes("--uppercase") || arguments.includes("-u")) {
-    lowerCase += upperCase;
-  }
-
-  if (arguments.includes("--symbols") || arguments.includes("-s")) {
-    lowerCase += symbols;
-  }
-
-  console.log(`Password length to use: ${lengthValue}`);
 }
 
 let password = "";
